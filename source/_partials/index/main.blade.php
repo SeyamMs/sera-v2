b5360902d7eab85e601be4f51d95104fd82af067
Mohammed Seyam <seyam.ms@gmail.com>
2024-08-18 20:28:27 +0300

    fix nav to top

--- source/_partials/index/main.blade.php
+++ source/_partials/index/main.blade.php
@@ -1,11 +1,15 @@
 <section
-    class="relative flex flex-col h-screen min-h-[30rem] items-center overflow-hidden bg-primary-950"
-    x-data="{ shown: false }"
+    class="relative flex flex-col h-screen min-h-[600px] items-center overflow-hidden bg-primary-950"
+    x-data="{ shown: false, showFixed: window.scrollY > 115 }"
     x-intersect.threshold.20="shown = true"
+    x-on:scroll.window="showFixed = window.scrollY > 115"
 >
-    {{-- fixed navbar --}}
-    <div class="fixed w-full">
-        <div class="lg:p-4 container relative p-2 mx-auto mb-8">
+    {{-- navbar --}}
+    <div
+        class="fixed top-0 right-0 z-50 w-full"
+        :class="{ 'bg-[#0F0F0F]': showFixed }"
+    >
+        <div class="lg:px-4 lg:py-6 container relative px-2 py-3 mx-auto">
             <div class="relative z-10 flex w-full">
                 <div class="md:justify-between flex justify-center w-full gap-4">
                     <div
@@ -111,113 +115,6 @@
 
         </div>
     </div>
-
-    {{-- navbar --}}
-    {{-- <div class="lg:p-4 container relative p-2 mx-auto mb-8">
-        <div class="relative z-10 flex w-full">
-            <div class="md:justify-between flex justify-center w-full gap-4">
-                <div
-                    class="md:block hidden w-40"
-                    x-show="shown"
-                    x-transition:enter-end="opacity-100 translate-x-0"
-                    x-transition:enter-start="opacity-0 translate-x-10"
-                    x-transition:enter="transition transform ease-out duration-300 delay-[300ms]"
-                    x-transition:leave-end="opacity-0 translate-x-10"
-                    x-transition:leave-start="opacity-100 translate-x-0"
-                    x-transition:leave="transition transform ease-in duration-300"
-                >
-                    <img
-                        class="object-scale-down w-full h-full"
-                        src="{{ $page->asset('images/white-logo.svg') }}"
-                        alt="white-logo"
-                    />
-                </div>
-
-                <div
-                    class="bg-opacity-15 backdrop-blur rounded-xl md:p-3 lg:p-4 flex p-2 bg-white"
-                    x-show="shown"
-                    x-transition:enter-end="opacity-100 translate-y-0"
-                    x-transition:enter-start="opacity-0 -translate-y-10"
-                    x-transition:enter="transition transform ease-out duration-300 delay-[900ms]"
-                    x-transition:leave-end="opacity-0 -translate-y-10"
-                    x-transition:leave-start="opacity-100 translate-y-0"
-                    x-transition:leave="transition transform ease-in duration-300"
-                >
-                    <div class="lg:gap-2 flex items-center gap-1">
-                        <button
-                            class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
-                            x-data
-                            x-on:click.prevent="$scroll('#about-section', {behavior: 'smooth', offset: -1})"
-                        >
-                            التعريف بالهاكاثون
-                        </button>
-                        <span class="md:block flex-shrink-0 hidden w-px h-4 bg-gray-100"></span>
-                        <button
-                            class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
-                            x-data
-                            x-on:click.prevent="$scroll('#goals-section', {behavior: 'smooth', offset: -1})"
-                        >
-                            الاهداف
-                        </button>
-                        <span class="md:block flex-shrink-0 hidden w-px h-4 bg-gray-100"></span>
-                        <button
-                            class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
-                            x-data
-                            x-on:click.prevent="$scroll('#paths-section', {behavior: 'smooth', offset: -1})"
-                        >
-                            المسارات
-                        </button>
-                        <span class="md:block flex-shrink-0 hidden w-px h-4 bg-gray-100"></span>
-                        <button
-                            class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
-                            x-data
-                            x-on:click.prevent="$scroll('#prizes-section', {behavior: 'smooth', offset: -1})"
-                        >
-                            الجوائز
-                        </button>
-                        <div class="w-28 lg:flex items-center hidden h-8">
-                            <button
-                                class="text-lg group hover:bg-[#6d5d8250] hover:ring-2 hover:ring-opacity-60 hover:ring-white font-semibold rounded-full flex items-center justify-between hover:justify-center p-1 h-8 w-28 bg-white hover:text-white text-[#230C44]"
-                                data-tf-popup="Z2f34azw"
-                            >
-                                <div class="flex justify-center flex-grow">
-                                    <span class="md:text-sm text-xs">
-                                        انضم الينا
-                                    </span>
-                                </div>
-                                <div
-                                    class="group-hover:hidden flex shrink-0 items-center justify-center size-7 rounded-full bg-[#230C44]">
-                                    <img
-                                        class="w-4"
-                                        src="{{ $page->asset('images/icons/lamp-light.svg') }}"
-                                        alt="lamp-light"
-                                    />
-                                </div>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <div
-                    class="md:block hidden w-40"
-                    x-show="shown"
-                    x-transition:enter-end="opacity-100 translate-x-0"
-                    x-transition:enter-start="opacity-0 -translate-x-10"
-                    x-transition:enter="transition transform ease-out duration-300 delay-[600ms]"
-                    x-transition:leave-end="opacity-0 -translate-x-10"
-                    x-transition:leave-start="opacity-100 translate-x-0"
-                    x-transition:leave="transition transform ease-in duration-300"
-                >
-                    <img
-                        class="object-scale-down w-full h-full"
-                        src="{{ $page->asset('images/electrathon-logo.svg') }}"
-                        alt="electrathod-logo"
-                    />
-                </div>
-            </div>
-        </div>
-
-    </div> --}}
 
     {{-- main section --}}
     <div class="py-14 lg:px-12 md:px-8 relative z-10 w-full h-full px-4">
--- source/_partials/path/main.blade.php
+++ source/_partials/path/main.blade.php
@@ -1,127 +1,138 @@
 <section
-    class="fixed top-0 right-0 flex flex-col w-full min-h-[510px] items-center overflow-hidden bg-primary-950"
-    x-data="{ shown: false }"
+    class="bg-primary-950 flex w-full min-h-[510px] overflow-hidden relative"
+    x-data="{ shown: false, showFixed: window.scrollY > 410 }"
     x-intersect.threshold.20="shown = true"
+    x-on:scroll.window="showFixed = window.scrollY > 410"
 >
-    <div class="lg:p-4 container relative p-2 mx-auto mb-8">
-        <div class="relative z-10 flex w-full">
-            <div class="md:justify-between flex justify-center w-full gap-4">
-                <div
-                    class="md:block hidden w-40"
-                    x-show="shown"
-                    x-transition:enter-end="opacity-100 translate-x-0"
-                    x-transition:enter-start="opacity-0 translate-x-10"
-                    x-transition:enter="transition transform ease-out duration-300 delay-[300ms]"
-                    x-transition:leave-end="opacity-0 translate-x-10"
-                    x-transition:leave-start="opacity-100 translate-x-0"
-                    x-transition:leave="transition transform ease-in duration-300"
-                >
-                    <img
-                        class="object-scale-down w-full h-full"
-                        src="{{ $page->asset('images/electrathon-logo.svg') }}"
-                        alt="electrathod-logo"
-                    />
-                </div>
+    <div
+        class="fixed top-0 right-0 z-50 w-full"
+        id="fixedNav"
+        :class="{ 'bg-[#0F0F0F]': showFixed }"
+    >
+        <div class="lg:px-4 lg:py-6 container relative px-2 py-3 mx-auto">
+            <div class="relative z-10 flex w-full">
+                <div class="md:justify-between flex justify-center w-full gap-4">
+                    <div
+                        class="md:block hidden w-40"
+                        x-show="shown"
+                        x-transition:enter-end="opacity-100 translate-x-0"
+                        x-transition:enter-start="opacity-0 translate-x-10"
+                        x-transition:enter="transition transform ease-out duration-300 delay-[300ms]"
+                        x-transition:leave-end="opacity-0 translate-x-10"
+                        x-transition:leave-start="opacity-100 translate-x-0"
+                        x-transition:leave="transition transform ease-in duration-300"
+                    >
+                        <img
+                            class="object-scale-down w-full h-full"
+                            src="{{ $page->asset('images/electrathon-logo.svg') }}"
+                            alt="electrathod-logo"
+                        />
+                    </div>
 
-                <div
-                    class="bg-opacity-15 backdrop-blur rounded-xl md:p-3 lg:p-4 flex p-2 bg-white"
+                    <div
+                        class="bg-opacity-15 backdrop-blur rounded-xl md:p-3 lg:p-4 flex p-2 bg-white"
+                        x-show="shown"
+                        x-transition:enter-end="opacity-100 translate-y-0"
+                        x-transition:enter-start="opacity-0 -translate-y-10"
+                        x-transition:enter="transition transform ease-out duration-300 delay-[900ms]"
+                        x-transition:leave-end="opacity-0 -translate-y-10"
+                        x-transition:leave-start="opacity-100 translate-y-0"
+                        x-transition:leave="transition transform ease-in duration-300"
+                    >
+                        <div class="lg:gap-2 flex items-center gap-1">
+                            <a
+                                class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
+                                href="/?to=about-section"
+                            >
+                                التعريف بالهاكاثون
+                            </a>
+                            <span class="md:block flex-shrink-0 hidden w-px h-4 bg-gray-100"></span>
+                            <a
+                                class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
+                                href="/?to=goals-section"
+                            >
+                                الاهداف
+                            </a>
+                            <span class="md:block flex-shrink-0 hidden w-px h-4 bg-gray-100"></span>
+                            <a
+                                class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
+                                href="/?to=paths-section"
+                            >
+                                المسارات
+                            </a>
+                            <span class="md:block flex-shrink-0 hidden w-px h-4 bg-gray-100"></span>
+                            <a
+                                class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
+                                href="/?to=prizes-section"
+                            >
+                                الجوائز
+                            </a>
+                        </div>
+                    </div>
+
+
+                    <div class="lg:flex items-center hidden w-40 h-16">
+                        <button
+                            class="text-lg group hover:bg-[#6d5d8250] hover:ring-2 hover:ring-opacity-60 hover:ring-white font-semibold rounded-full flex items-center justify-between hover:justify-center p-1 h-16 w-full bg-white hover:text-white text-[#230C44]"
+                            data-tf-popup="Z2f34azw"
+                            x-show="shown"
+                            x-transition:enter-end="opacity-100 translate-x-0"
+                            x-transition:enter-start="opacity-0 -translate-x-10"
+                            x-transition:enter="transition transform ease-out duration-300 delay-[600ms]"
+                            x-transition:leave-end="opacity-0 -translate-x-10"
+                            x-transition:leave-start="opacity-100 translate-x-0"
+                            x-transition:leave="transition transform ease-in duration-300"
+                        >
+                            <span class="px-4">
+                                سجل الان
+                            </span>
+                            <div
+                                class="group-hover:hidden flex items-center justify-center size-[50px] rounded-full bg-[#230C44] group-hover:bg-[#F5F9FA]">
+                                <img
+                                    src="{{ $page->asset('images/icons/lamp-light.svg') }}"
+                                    alt="lamp-light"
+                                />
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="fixed top-0 right-0 flex flex-col w-full min-h-[510px]">
+        <div class="flex flex-col flex-grow w-full h-full">
+            <div
+                class="py-14 lg:px-12 md:px-8 relative z-10 flex items-center justify-center flex-grow w-full h-full px-4">
+                <h1
+                    class="lg:text-[70px] md:text-[48px] text-[36px] mb-8 font-semibold text-white"
                     x-show="shown"
                     x-transition:enter-end="opacity-100 translate-y-0"
                     x-transition:enter-start="opacity-0 -translate-y-10"
-                    x-transition:enter="transition transform ease-out duration-300 delay-[900ms]"
+                    x-transition:enter="transition transform ease-out duration-300 delay-[1500ms]"
                     x-transition:leave-end="opacity-0 -translate-y-10"
                     x-transition:leave-start="opacity-100 translate-y-0"
                     x-transition:leave="transition transform ease-in duration-300"
                 >
-                    <div class="lg:gap-2 flex items-center gap-1">
-                        <a
-                            class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
-                            href="/?to=about-section"
-                        >
-                            التعريف بالهاكاثون
-                        </a>
-                        <span class="md:block flex-shrink-0 hidden w-px h-4 bg-gray-100"></span>
-                        <a
-                            class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
-                            href="/?to=goals-section"
-                        >
-                            الاهداف
-                        </a>
-                        <span class="md:block flex-shrink-0 hidden w-px h-4 bg-gray-100"></span>
-                        <a
-                            class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
-                            href="/?to=paths-section"
-                        >
-                            المسارات
-                        </a>
-                        <span class="md:block flex-shrink-0 hidden w-px h-4 bg-gray-100"></span>
-                        <a
-                            class="md:text-sm flex-shrink-0 p-2 text-xs text-white"
-                            href="/?to=prizes-section"
-                        >
-                            الجوائز
-                        </a>
-                    </div>
-                </div>
+                    تطوير منصات لتعزيز التفاعل والرضا لدى العملاء
+                </h1>
 
-
-                <div class="lg:flex items-center hidden w-40 h-16">
-                    <button
-                        class="text-lg group hover:bg-[#6d5d8250] hover:ring-2 hover:ring-opacity-60 hover:ring-white font-semibold rounded-full flex items-center justify-between hover:justify-center p-1 h-16 w-full bg-white hover:text-white text-[#230C44]"
-                        data-tf-popup="Z2f34azw"
-                        x-show="shown"
-                        x-transition:enter-end="opacity-100 translate-x-0"
-                        x-transition:enter-start="opacity-0 -translate-x-10"
-                        x-transition:enter="transition transform ease-out duration-300 delay-[600ms]"
-                        x-transition:leave-end="opacity-0 -translate-x-10"
-                        x-transition:leave-start="opacity-100 translate-x-0"
-                        x-transition:leave="transition transform ease-in duration-300"
-                    >
-                        <span class="px-4">
-                            سجل الان
-                        </span>
-                        <div
-                            class="group-hover:hidden flex items-center justify-center size-[50px] rounded-full bg-[#230C44] group-hover:bg-[#F5F9FA]">
-                            <img
-                                src="{{ $page->asset('images/icons/lamp-light.svg') }}"
-                                alt="lamp-light"
-                            />
-                        </div>
-                    </button>
-                </div>
             </div>
         </div>
 
-    </div>
-
-    <div class="flex flex-col flex-grow w-full h-full">
-        <div class="py-14 lg:px-12 md:px-8 relative z-10 flex items-center justify-center flex-grow w-full h-full px-4">
-            <h1
-                class="lg:text-[70px] md:text-[48px] text-[36px] mb-8 font-semibold text-white"
-                x-show="shown"
-                x-transition:enter-end="opacity-100 translate-y-0"
-                x-transition:enter-start="opacity-0 -translate-y-10"
-                x-transition:enter="transition transform ease-out duration-300 delay-[1500ms]"
-                x-transition:leave-end="opacity-0 -translate-y-10"
-                x-transition:leave-start="opacity-100 translate-y-0"
-                x-transition:leave="transition transform ease-in duration-300"
-            >
-                تطوير منصات لتعزيز التفاعل والرضا لدى العملاء
-            </h1>
-
+        <div class="absolute top-0 left-0 z-0 w-full h-full">
+            <div class="bg-opacity-10 absolute top-0 left-0 z-0 w-full h-full">
+                <img
+                    class="object-cover object-center w-full h-full"
+                    src="{{ $page->asset('images/paths/main.jpg') }}"
+                    alt="main-section"
+                />
+            </div>
+        </div>
+        <div class="backdrop-blur-sm bg-black/10 absolute top-0 left-0 z-0 w-full h-full">
         </div>
     </div>
 
-    <div class="absolute top-0 left-0 z-0 w-full h-full">
-        <div class="bg-opacity-10 absolute top-0 left-0 z-0 w-full h-full">
-            <img
-                class="object-cover object-center w-full h-full"
-                src="{{ $page->asset('images/paths/main.jpg') }}"
-                alt="main-section"
-            />
-        </div>
-    </div>
-    <div class="backdrop-blur-sm bg-black/10 absolute top-0 left-0 z-0 w-full h-full">
-    </div>
+
 </section>
-<div class="min-h-[510px] w-0"></div>
