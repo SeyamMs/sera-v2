6c9e17517b8bc8ae36f986a588658ead47d73135
Mohammed Seyam <seyam.ms@gmail.com>
2024-08-21 05:23:21 +0300

    remove font.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,7 +7,6 @@ mix.setPublicPath("source/assets/build")
 mix.jigsaw()
 	.js("source/_assets/js/main.js", "js")
 	.css("source/_assets/css/main.css", "css", [require("postcss-import"), require("tailwindcss")])
-	.css("source/_assets/css/font.css", "css", [])
 	.options({
 		processCssUrls: false,
 	})
